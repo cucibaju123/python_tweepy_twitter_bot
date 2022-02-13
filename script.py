a8f70192dd5ff01c7c3ea405a931843a2931b69d
cucibaju123 <cucibaju123@gmail.com>
2022-02-13 12:16:07 +0700

    Fixed twitter secrets returning None
    Twitter access tokens returns None because Twitter API app type
    permission is set to "READ only". To resolve, set OAuth 2.0, set
    to "READ/WRITE", then regenerate access token. Dont forget to update
    .env file

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import tweepy
 import os
+from dotenv import load_dotenv
 
 
 class TwitterSecrets:
     """Class that holds Twitter Secrets"""
 
     def __init__(self):
-        self.API_KEY = os.environ.get("TWITTER_API_KEY")
-        self.API_SECRET = os.environ.get("TWITTER_API_SECRET")
-        self.ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-        self.ACCESS_SECRET = os.environ.get("TWITTER_ACCESS_SECRET")
-        self.BEARER_TOKEN = os.environ.get("TWITTER_BEARER_TOKEN")
+        # load keys from .env file, so we can use os.environ.get()
+        load_dotenv()
+        self.TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
+        self.TWITTER_API_SECRET = os.environ.get("TWITTER_API_SECRET")
+        self.TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
+        self.TWITTER_ACCESS_SECRET = os.environ.get("TWITTER_ACCESS_SECRET")
+        self.TWITTER_BEARER_TOKEN = os.environ.get("TWITTER_BEARER_TOKEN")
 
         # Tests if keys are present
         for key, secret in self.__dict__.items():
@@ -20,9 +23,9 @@ class TwitterSecrets:
 secrets = TwitterSecrets()
 
 # authenticatng to use Twitter API key
-auth = tweepy.OAuthHandler(secrets.API_KEY, secrets.API_SECRET)
+auth = tweepy.OAuthHandler(secrets.TWITTER_API_KEY, secrets.TWITTER_API_SECRET)
 # Set access token and secret so all API requests will be on our account's behalf
-auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET)
+auth.set_access_token(secrets.TWITTER_ACCESS_TOKEN, secrets.TWITTER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
 # Generate text tweet
